4f09690e174d168183c9164aa18c4fdb80e3bcbf
adrion <frossardcedric@gmail.com>
2016-06-21 08:33:54 +0200

    prevent page styles

--- app/scripts/honcode.js
+++ app/scripts/honcode.js
@@ -24,26 +24,6 @@ var honcode = {
     return subdomain !== '' ? subdomain + '.' + domain : domain;
   },
 
-  displayHONcodeStatus: function(link) {
-    hon_listHON.checkURL(hon_listHON.formatHREF(link)).then(function(code) {
-      var HONcodeCertificateLink = code;
-      var icon = document.getElementById('honstatus');
-      var certificate = document.getElementById('certificateLink');
-      var langue = navigator.language.substring(0,2);
-
-      if (HONcodeCertificateLink === '' ||
-        HONcodeCertificateLink === undefined) {
-        certificate.title = 'No HonCode certification';
-        icon.src = 'images/honcode/hon-invalid-large.png';
-      } else {
-        icon.src = 'images/honcode/hon-valid-large.png';
-        certificate.title = 'HonCode certified';
-        certificate.href =
-          'http://services.hon.ch/cgi-bin/Plugin/redirect.pl?' +
-          HONcodeCertificateLink + ' +' + langue;
-      }
-    });
-  },
   contentHONcodeStatus: function(target, link) {
     hon_listHON.checkURL(hon_listHON.formatHREF(link)).then(function(code) {
       var HONcodeCertificateLink = code;
