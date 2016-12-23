ee9e98135ab723ab996068866bf5d1a41b440f8d
William Belle <william.belle@gmail.com>
2016-12-23 20:04:32 +0100

    request list in https

--- app/scripts/utils/listHON.js
+++ app/scripts/utils/listHON.js
@@ -15,7 +15,7 @@
 var hon_listHON =
 {
   // Url de la liste des sites certifies
-  url: 'http://www.hon.ch/HONcode/Plugin/listeMD5.txt',
+  url: 'https://www.hon.ch/HONcode/Plugin/listeMD5.txt',
 
   // Initialisation de la HashMap
   initSiteList: false,
