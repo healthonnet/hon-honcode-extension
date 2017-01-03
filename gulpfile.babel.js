ea438eb7c647261fa69e3fbd86af99219fcb103f
Pierre REPETTO-ANDIPATIN <pierre@anthillsolutions.ch>
2017-01-03 13:26:14 +0100

    Refactor gulp (#34)
    * refactor: gulp can now have different browsers
    
    * fix: firefox popup working
    
    * fix: gulp package

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,4 +1,3 @@
-// Generated on 2016-02-09 using generator-chrome-extension 0.5.2
 require('babel-core/register');
 
 import gulp from 'gulp';
@@ -7,18 +6,16 @@ import del from 'del';
 import casperJs from 'gulp-casperjs';
 import runSequence from 'run-sequence';
 import {stream as wiredep} from 'wiredep';
-import jscs from 'gulp-jscs';
-import jshint from 'gulp-jshint';
-import download from 'gulp-download';
-import decompress from 'gulp-decompress';
-import mergeJson from 'gulp-merge-json';
-import mergeStream from 'merge-stream';
 import lazypipe from 'lazypipe';
-import replace from 'gulp-replace';
+import {argv} from 'yargs';
 
 const $ = gulpLoadPlugins();
-const argv = require('yargs').argv;
 
+/* Define destination directory */
+let dest = 'dist/chrome';
+if (argv.firefox) {
+  dest = 'dist/firefox';
+}
 
 gulp.task('extras', () => {
   return gulp.src([
@@ -30,7 +27,7 @@ gulp.task('extras', () => {
   ], {
     base: 'app',
     dot: true,
-  }).pipe(argv.firefox ? gulp.dest('distFirefox') : gulp.dest('dist'));
+  }).pipe(gulp.dest(dest));
 });
 
 gulp.task('lint', () => {
@@ -40,8 +37,8 @@ gulp.task('lint', () => {
     '!app/scripts/utils/*.js',
     '!app/lib/**/*.js',
   ])
-    .pipe(jshint('.jshintrc'))
-    .pipe(jshint.reporter('default'));
+    .pipe($.jshint('.jshintrc'))
+    .pipe($.jshint.reporter('default'));
 });
 
 gulp.task('jscs', () => {
@@ -51,8 +48,8 @@ gulp.task('jscs', () => {
     '!app/scripts/utils/tld.js',
     '!app/lib/**/*.js',
   ])
-    .pipe(jscs('.jscsrc'))
-    .pipe(jscs.reporter());
+    .pipe($.jscs('.jscsrc'))
+    .pipe($.jscs.reporter());
 });
 
 gulp.task('images', () => {
@@ -68,10 +65,9 @@ gulp.task('images', () => {
       console.log(err);
       this.end();
     })))
-    .pipe(
-      argv.firefox ? gulp.dest('distFirefox/images') : gulp.dest('dist/images')
-    );
+    .pipe(gulp.dest(dest + '/images'));
 });
+
 gulp.task('styles', () => {
   const variables = {};
   variables.chromePrefix =
@@ -99,12 +95,13 @@ gulp.task('html', ['styles'], () => {
     .pipe(assets.restore())
     .pipe($.useref())
     .pipe($.if('*.html', $.minifyHtml({conditionals: true, loose: true})))
-    .pipe(argv.firefox ? gulp.dest('distFirefox') : gulp.dest('dist'));
+    .pipe(gulp.dest(dest));
 });
 
-gulp.task('chromeManifest', () => {
+gulp.task('manifest', () => {
   const manifestPath =
     argv.firefox ? 'app/manifest.firefox.json' : 'app/manifest.json';
+
   return gulp.src(manifestPath)
   .pipe($.rename('manifest.json'))
   .pipe($.chromeManifest({
@@ -116,55 +113,66 @@ gulp.task('chromeManifest', () => {
   .pipe($.if(/^(.{0,3}|.*(?!\.min).{4})\.js$/, $.sourcemaps.init()))
   .pipe($.if(/^(.{0,3}|.*(?!\.min).{4})\.js$/, $.uglify()))
   .pipe($.if(/^(.{0,3}|.*(?!\.min).{4})\.js$/, $.sourcemaps.write('.')))
-  .pipe(argv.firefox ? gulp.dest('distFirefox') : gulp.dest('dist'));
+  .pipe(gulp.dest(dest));
 });
 
 gulp.task('locales', () => {
-  return download('https://localise.biz:443/api/export/archive/json.zip?' +
+  return $.download('https://localise.biz:443/api/export/archive/json.zip?' +
     'key=dd5d1fa46f1ba7941659779f6423e38e&format=chrome')
-    .pipe(decompress({strip: 1}))
-    .pipe(argv.firefox ? gulp.dest('distFirefox') : gulp.dest('dist'));
+    .pipe($.decompress({strip: 1}))
+    .pipe(gulp.dest(dest));
 });
 
 var alterJson = lazypipe()
-  .pipe(replace, /("\w\w"):("[^"]*")/g, '$1: { "message": $2 }');
+  .pipe($.replace, /("\w\w"):("[^"]*")/g, '$1: { "message": $2 }')
+  .pipe($.mergeJson, 'messages.json');
 
 gulp.task('lang-de', () => {
-  return mergeStream(gulp.src('./app/lib/country-code/data/de/country.json')
-    .pipe(alterJson()), gulp.src('dist/_locales/de/messages.json'))
-    .pipe(mergeJson('messages.json'))
-    .pipe(gulp.dest('dist/_locales/de'));
+  return gulp.src([
+      './app/lib/country-code/data/de/country.json',
+      dest + '/_locales/de/messages.json',
+    ])
+    .pipe(alterJson())
+    .pipe(gulp.dest(dest + '/_locales/de'));
 });
 
 gulp.task('lang-en', () => {
-  return mergeStream(gulp.src('./app/lib/country-code/data/en/country.json')
-    .pipe(alterJson()), gulp.src('dist/_locales/en/messages.json'))
-    .pipe(mergeJson('messages.json'))
-    .pipe(gulp.dest('dist/_locales/en'));
+  return gulp.src([
+      './app/lib/country-code/data/en/country.json',
+      dest + '/_locales/en/messages.json',
+    ])
+    .pipe(alterJson())
+    .pipe(gulp.dest(dest + '/_locales/en'));
 });
 
 gulp.task('lang-es', () => {
-  return mergeStream(gulp.src('./app/lib/country-code/data/es/country.json')
-    .pipe(alterJson()), gulp.src('dist/_locales/es/messages.json'))
-    .pipe(mergeJson('messages.json'))
-    .pipe(gulp.dest('dist/_locales/es'));
+  return gulp.src([
+      './app/lib/country-code/data/es/country.json',
+      dest + '/_locales/es/messages.json',
+    ])
+    .pipe(alterJson())
+    .pipe(gulp.dest(dest + '/_locales/es'));
 });
 
 gulp.task('lang-fr', () => {
-  return mergeStream(gulp.src('./app/lib/country-code/data/fr/country.json')
-    .pipe(alterJson()), gulp.src('dist/_locales/fr/messages.json'))
-    .pipe(mergeJson('messages.json'))
-    .pipe(gulp.dest('dist/_locales/fr'));
+  return gulp.src([
+      './app/lib/country-code/data/fr/country.json',
+      dest + '/_locales/fr/messages.json',
+    ])
+    .pipe(alterJson())
+    .pipe(gulp.dest(dest + '/_locales/fr'));
 });
 
 gulp.task('lang-it', () => {
-  return mergeStream(gulp.src('./app/lib/country-code/data/it/country.json')
-    .pipe(alterJson()), gulp.src('dist/_locales/it/messages.json'))
-    .pipe(mergeJson('messages.json'))
-    .pipe(gulp.dest('dist/_locales/it'));
+  return gulp.src([
+      './app/lib/country-code/data/it/country.json',
+      dest + '/_locales/it/messages.json',
+    ])
+    .pipe(alterJson())
+    .pipe(gulp.dest(dest + '/_locales/it'));
 });
 
-gulp.task('clean', del.bind(null, ['.tmp', 'dist', 'distFirefox']));
+gulp.task('clean', del.bind(null, ['.tmp', dest]));
 
 gulp.task('watch', ['lint', 'html'], () => {
   $.livereload.listen();
@@ -183,17 +191,17 @@ gulp.task('watch', ['lint', 'html'], () => {
 });
 
 gulp.task('size', () => {
-  return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
+  return gulp.src(dest + '/**/*').pipe($.size({title: 'build', gzip: true}));
 });
 
 gulp.task('flags', () => {
   return gulp.src('app/lib/flag-css/dist/flags/*.svg')
-    .pipe(gulp.dest('dist/flags'));
+    .pipe(gulp.dest(dest + '/flags'));
 });
 
 gulp.task('fonts', () => {
   return gulp.src('app/lib/font-awesome/fonts/*')
-    .pipe(gulp.dest('dist/fonts'));
+    .pipe(gulp.dest(dest + '/fonts'));
 });
 
 gulp.task('wiredep', () => {
@@ -202,36 +210,6 @@ gulp.task('wiredep', () => {
       ignorePath: /^(\.\.\/)*\.\./,
     }))
     .pipe(gulp.dest('app'));
-});
-
-gulp.task('package', () => {
-  var manifest;
-  if (argv.firefox) {
-    manifest = require('./distFirefox/manifest.json');
-    del.sync(['distFirefox/README.txt', 'distFirefox/**/*.map']);
-    return gulp.src('distFirefox/**')
-        .pipe($.zip('HONcode-Toolbar-v' + manifest.version + '-Firefox.zip'))
-        .pipe(gulp.dest('package'));
-  }
-  manifest = require('./dist/manifest.json');
-  del.sync(['dist/README.txt', 'dist/**/*.map']);
-  return gulp.src('dist/**')
-      .pipe($.zip('HONcode-Toolbar-v' + manifest.version + '-Chrome.zip'))
-      .pipe(gulp.dest('package'));
-});
-
-gulp.task('test', () => {
-  runSequence(['lint', 'jscs']);
-  gulp.src('test/casper.js')
-    .pipe(casperJs({command: 'test --web-security=no'}));
-  // Run casperjs test
-});
-
-gulp.task('build', (cb) => {
-  runSequence(
-    ['html', 'images', 'extras'],
-    'chromeManifest', 'flags', 'fonts',
-    'lang', 'size', cb);
 });
 
 gulp.task('lang', ['locales'], (cb) => {
@@ -245,8 +223,29 @@ gulp.task('lang', ['locales'], (cb) => {
   );
 });
 
-gulp.task('chrome-prod', ['clean'], (cb) => {
-  runSequence('build', 'package', cb);
+gulp.task('package', ['build'], () => {
+  var browser = 'Chrome';
+  if (argv.firefox) {
+    browser = 'Firefox';
+  }
+  var manifest = require('./' + dest + '/manifest.json');
+  del.sync([dest + '/README.txt', dest + '/**/*.map']);
+  return gulp.src(dest + '/**')
+      .pipe($.zip('HONcode-Toolbar-v' + manifest.version + '-' +
+        browser + '.zip'))
+      .pipe(gulp.dest('package'));
+});
+
+gulp.task('test', ['lint', 'jscs'], () => {
+  gulp.src('test/casper.js')
+    .pipe(casperJs({command: 'test --web-security=no'}));
+});
+
+gulp.task('build', (cb) => {
+  runSequence(
+    ['html', 'images', 'extras'],
+    'manifest', 'flags', 'fonts',
+    'lang', 'size', cb);
 });
 
 gulp.task('default', ['clean'], cb => {
