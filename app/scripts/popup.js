c830919b9a7e680f7902eb4f6739ed25a2bd68af
William Belle <william.belle@gmail.com>
2017-02-19 10:38:00 +0100

    add donate link in footer

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -11,5 +11,14 @@ chrome.tabs.query(query, function(tabs) {
   $('#q').attr('placeholder',
     chrome.i18n.getMessage('khresmoiPlaceholder'));
   $('#searchSubmit').text(chrome.i18n.getMessage('khresmoiSearch'));
+  $('#donate').append(
+    $('<a>', {
+      target: '_blank',
+      href: 'https://www.paypal.com/cgi-bin/webscr' +
+        '?cmd=_s-xclick&hosted_button_id=YH3ZJP32PN4PS',
+    }).text(' ' + chrome.i18n.getMessage('donate')).prepend(
+      $('<i>', {class: 'fa fa-heart'})
+    )
+  );
   honcode.setBadges(currentTab.url);
 });
