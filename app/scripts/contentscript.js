14f2ede0ee7cc1e0efb25cb64af438505776b9e8
cedric frossard <adrion.dev@gmail.com>
2017-10-09 11:21:48 +0200

    apply same fix of K.Trust Indicator

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -82,6 +82,7 @@ var updateLinks = function() {
   // Get links
   var links = [];
   var hrefSelector = '';
+  var timeToWait = 1;
   var honCodeRequested = 0;
   var honLogoSize = 'honcode-logo-tall';
   // Match Google
@@ -91,6 +92,7 @@ var updateLinks = function() {
   // Match Yahoo
   else if (window.location.host.indexOf('yahoo') > -1) {
     hrefSelector = '#web div.compTitle h3.title>a';
+    timeToWait = 1000;
   }
   // Match Bing
   else if (window.location.host.indexOf('bing') > -1) {
@@ -106,37 +108,38 @@ var updateLinks = function() {
     hrefSelector = '.bulletlist a';
     honLogoSize = 'honcode-logo-wide';
   }
-
-  var nodeList = document.querySelectorAll(hrefSelector);
-  for (var i = 0; i < nodeList.length; ++i) {
-    links[i] = nodeList[i].href;
-  }
-  links.forEach(function(link, index) {
-    var honLogo = $(nodeList.item(index)).parent();
-
-    var logoId = 'honLogo_' + index;
-
-    var honCodeLogo = '<div target=\'_blank\' id="' + logoId +
-      '" class="hon certificateLink ' + honLogoSize + '"></div>';
-
-    if (honLogo.children('.certificateLink').length === 0) {
-      // Normalize Search Engine parents' behaviors
-      honLogo.parent().css('overflow','visible');
-      honLogo.parent().css('position','relative');
-      honLogo.prepend(honCodeLogo);
-      honcode.contentHONcodeStatus(honLogo.children('.certificateLink'), link);
-
-      // Add onClick listener
-      $('#' + logoId).one('click', function(e) {
-        requestHonCode(e, link);
-      });
+  setTimeout(function() {
+    var nodeList = document.querySelectorAll(hrefSelector);
+    for (var i = 0; i < nodeList.length; ++i) {
+      links[i] = nodeList[i].href;
     }
+    links.forEach(function(link, index) {
+      var honLogo = $(nodeList.item(index)).parent();
 
-    honCodeRequested++;
-    if (honCodeRequested === links.length) {
-      deferred.resolve();
-    }
-  });
+      var logoId = 'honLogo_' + index;
+
+      var honCodeLogo = '<div target=\'_blank\' id="' + logoId +
+        '" class="hon certificateLink ' + honLogoSize + '"></div>';
+
+      if (honLogo.children('.certificateLink').length === 0) {
+        // Normalize Search Engine parents' behaviors
+        honLogo.parent().css('overflow','visible');
+        honLogo.parent().css('position','relative');
+        honLogo.prepend(honCodeLogo);
+        honcode.contentHONcodeStatus(honLogo.children('.certificateLink'), link);
+
+        // Add onClick listener
+        $('#' + logoId).one('click', function(e) {
+          requestHonCode(e, link);
+        });
+      }
+
+      honCodeRequested++;
+      if (honCodeRequested === links.length) {
+        deferred.resolve();
+      }
+    });
+  }, timeToWait);
 
   return deferred.promise();
 };
