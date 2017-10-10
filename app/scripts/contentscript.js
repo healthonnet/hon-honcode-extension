9f0f02054e68a6411e78749de3a7ed069260ed74
cedric frossard <adrion.dev@gmail.com>
2017-10-10 08:04:12 +0200

    handle infinite scroll

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -105,6 +105,9 @@ var updateLinks = function() {
   else if (window.location.host.indexOf('duckduckgo') > -1) {
     hrefSelector = 'h2.result__title>a.result__a';
     timeToWait = 1000;
+    $('head').bind('DOMSubtreeModified', function() {
+      updateLinks();
+    });
   }
   // Match Wikipedia
   else if (!isFirefox && window.location.host.indexOf('wikipedia') > -1) {
